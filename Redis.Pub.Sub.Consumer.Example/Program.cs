d986534574069305d8a536b9d4cc499adf7a60a2
tahapek5454 <tahapek5454@gmail.com>
2023-03-19 12:30:34 +0300

    feat: add pub.sub

---
+++ Redis.Pub.Sub.Consumer.Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+
+// ConnectionMultiplexer sınıfı üzerinden Redis sunucusuna bir bağlantı oluşturunuz.
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+// Devamında bu bağlantı üzerinden subscriber oluşturacağız
+ISubscriber subscriber = connection.GetSubscriber();
+
+// subscribe olarak consume islemini gerceklestirebilriz
+await subscriber.SubscribeAsync("mychannel.*", (channel, value) =>
+{
+    // mesaj string olarak gelecektir
+    Console.WriteLine(value);
+
+});
+
+Console.Read();
---
+++ Redis.Pub.Sub.Example/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using StackExchange.Redis;
+
+// ConnectionMultiplexer sınıfı üzerinden Redis sunucusuna bir bağlantı oluşturunuz.
+ConnectionMultiplexer connection =  await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+// Devamında bu bağlantı üzerinden subscriber oluşturacağız
+ISubscriber subscriber =  connection.GetSubscriber();
+
+// mesaj olusturma
+
+while (true)
+{
+    Console.Write("Mesajinizi Giriniz : ");
+    string message = Console.ReadLine();
+
+    // publish etme islemi
+    await subscriber.PublishAsync("mychannel.apple", message);
+}
