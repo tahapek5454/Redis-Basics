10bcadce576ba8b7c6904a4683fad0bea23becc6
tahapek5454 <tahapek5454@gmail.com>
2023-03-19 11:40:35 +0300

    feat: add distributed and inmemory caching

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Distributed.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+
+        [HttpPost("{name}")]
+        public async Task<IActionResult> Post([FromRoute] string name) 
+        {
+            // absolute ve slide configurasyonlarini da 3. parametere olarak verelim
+            await _distributedCache.SetStringAsync("name", name, new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.UtcNow.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+
+            });
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var value = await _distributedCache.GetStringAsync("name");
+
+            return Ok(value);
+        }
+    }
+}
---
+++ InMemoryCaching/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //IMemoryCache interface!inden yararlanacagiz
+        private readonly IMemoryCache _memoryCache;
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        //[HttpPost("{name}")]
+        //public void Post([FromRoute] string name)
+        //{
+        //    _memoryCache.Set("name", name);
+
+        //}
+
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    if(_memoryCache.TryGetValue<string>("name", out string name)) return name;
+        //    return "";
+        //}
+
+        [HttpPost]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                // kesin kalma degeri 30 sn fakat islem yapilirsa 3 sn 3 sn arttir
+                AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(3)
+            });
+
+        }
+
+        [HttpGet]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");  
+
+        }
+
+
+    }
+}
